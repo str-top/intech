7df64d9f853f39493c28b1f580329a29b015195e
Преподаватель Павел Игоревич <rudnikov.paul@ya.ru>
2025-11-29 10:07:32 +0500

    Update 5.1.ino

--- 5.1.ino
+++ 5.1.ino
@@ -1,42 +1,41 @@
-#include <DHT.h>
+//stored pins in variables
 
-#define DHTPIN 2          // DHT11 data pin
-#define DHTTYPE DHT11
-
+#define gasSensor A0
 #define buzzer 0
 #define ledGreen 1
 #define ledRed 2
-
-DHT dht(DHTPIN, DHTTYPE);
-
-// Trigger temperature
-#define TEMP_LIMIT 35
+#define HIGH 600
 
 void setup() {
-  dht.begin();
 
-  pinMode(buzzer, OUTPUT);
-  pinMode(ledGreen, OUTPUT);
-  pinMode(ledRed, OUTPUT);
-}
+   //Initialising all pins
+
+   pinMode(gasSensor, INPUT);
+   pinMode(buzzer, OUTPUT);
+   pinMode(ledGreen, OUTPUT);
+   pinMode(ledRed, OUTPUT);
+  }
 
 void loop() {
-  float t = dht.readTemperature();
+  //Read data from the sensor
+int gas_value = analogRead(gasSensor);
 
-  // If reading failed skip this loop
-  if (isnan(t)) {
-    return;
-  }
 
-  if (t > TEMP_LIMIT) {
-    tone(buzzer, 1000, 500);
-    digitalWrite(ledRed, HIGH);
-    digitalWrite(ledGreen, LOW);
-  } else {
-    noTone(buzzer);
-    digitalWrite(ledGreen, HIGH);
-    digitalWrite(ledRed, LOW);
-  }
+//check data from sensor if there is smoke, if will execute otherwise else will execute
+if(gas_value > HIGH)
+{
+  tone(buzzer,1000,500);
+  digitalWrite(ledRed, HIGH);
+  digitalWrite(ledGreen,LOW);
 
-  delay(500);
+
+}
+else
+{
+  noTone(buzzer);
+  digitalWrite(ledGreen,HIGH);
+  digitalWrite(ledRed, LOW);
+}
+delay(200);
+
 }
